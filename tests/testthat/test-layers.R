bb5dca39011b4d2b135cf10b860d7f8c3dbd5b8e
Barret Schloerke <schloerke@gmail.com>
2017-09-15 11:24:21 -0400

    add testing for cog_spec

--- tests/testthat/test-layers.R
+++ tests/testthat/test-layers.R
@@ -6,26 +6,47 @@ library(ggplot2)
 
 test_that("ggplot2 layers", {
 
-  p <- qplot(Sepal.Length, Sepal.Width, data = iris) +
+  p <- ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
+    geom_point() +
     geom_smooth(method = "auto")
 
-  expect_silent({
-    ignore <- plot_cogs(p, layers = TRUE)
-  })
-  expect_silent({
-    ignore <- plot_cogs(p, layers = c(FALSE, TRUE))
-  })
-  expect_silent({
-    ignore <- plot_cogs(p, layers = c(TRUE, FALSE))
-  })
-  expect_silent({
-    ignore <- plot_cogs(p, layers = c(TRUE, TRUE))
-  })
+  check_plot_count <- function(spec, count) {
+    expect_silent({
+      pc <- plot_cogs(p, spec = spec)
+    })
+    expect_equal(length(pc), count)
+  }
+
+  check_plot_count(TRUE, 2)
+  check_plot_count(c(FALSE, TRUE), 1)
+  check_plot_count(c(TRUE, FALSE), 1)
+  check_plot_count(c(TRUE, TRUE), 2)
 
   expect_error({
-    plot_cogs(p, layers = FALSE)
+    plot_cogs(p, spec = FALSE)
   })
   expect_error({
-    plot_cogs(p, layers = c(TRUE, TRUE, TRUE))
+    plot_cogs(p, spec = c(TRUE, TRUE, TRUE))
   })
+
+
+  check_plot_count(list(cog_spec(), cog_spec()), 2)
+  check_plot_count(list(cog_spec(), TRUE), 2)
+  check_plot_count(list(cog_spec(), FALSE), 1)
+  check_plot_count(list(cog_spec(scagnostics = FALSE), TRUE), 2)
+  check_plot_count(list(cog_spec(keep = FALSE), TRUE), 1)
+
+  expect_equal(
+    cog_spec()$remove,
+    character(0)
+  )
+  expect_equal(
+    cog_spec(scagnostics = FALSE)$remove,
+    c("scagnostics")
+  )
+  expect_equal(
+    cog_spec(scagnostics = TRUE)$remove,
+    character(0)
+  )
+
 })

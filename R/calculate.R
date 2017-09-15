89d1a28cdd7a0483562830a71e9bdefaccca86a6
Barret Schloerke <schloerke@gmail.com>
2017-09-15 11:50:44 -0400

    add layer_count s3 method

--- R/calculate.R
+++ R/calculate.R
@@ -44,7 +44,31 @@ plot_class.ggplot <- function(p) {
 }
 
 
-plot_cogs <- function(p, ..., verbose = FALSE) {
+#' Plot layer count
+#'
+#' Retrieves the number of layers in a given plot
+#' @param p plot object
+#' @return number
+#' @export
+#' @rdname layer_count
+#' @examples
+#' library(ggplot2)
+#' p <- ggplot(iris, aes(Sepal.Length, Sepal.Width)) + geom_point()
+#' layer_count(p) # 1
+#' layer_count(p + geom_smooth(method = "lm") + geom_density_2d()) # 3
+layer_count <- function(p) {
+  UseMethod("layer_count")
+}
+#' @export
+#' @rdname layer_count
+layer_count.default <- function(p) {
+  stop("Please implement `layer_count.", class(p)[1], "(p)`")
+}
+#' @export
+#' @rdname layer_count
+layer_count.ggplot <- function(p) {
+  length(p$layers)
+}
 
   plot_class_val <- plot_class(p)
   layer_info <- get_layer_data(p, ...)
